d718df90f6d24ad7fa47a2a876bbf2578f0cd101
hyj <2653957671@qq.com>
2023-06-26 17:41:23 +0800

    1

--- src/views/welcom/index.vue
+++ src/views/welcom/index.vue
@@ -7,11 +7,12 @@
         labelClassName="welcom-label"
         contentClassName="welcom-content"
       >
+     
         <el-descriptions-item :label="$t('useCommonAll.jobNumber')">kooriookami</el-descriptions-item>
         <el-descriptions-item :label="$t('useCommonAll.name')" >hah</el-descriptions-item>
         <el-descriptions-item :label="$t('useCommonAll.sex')" >女</el-descriptions-item>
         <el-descriptions-item :label="$t('useCommonAll.entryStatus')" >是</el-descriptions-item>
-        <el-descriptions-item :label="$t('useCommonAll.position')" >管理员</el-descriptions-item>
+        <el-descriptions-item :label="$t('useCommonAll.position')" ><el-tag >管理员</el-tag></el-descriptions-item>
         <el-descriptions-item :label="$t('useCommonAll.phone')" >18877767787</el-descriptions-item>
         <el-descriptions-item :label="$t('useCommonAll.email')" >666@qq.com</el-descriptions-item>
         <el-descriptions-item :label="$t('useCommonAll.birthday')" >1998/09/90</el-descriptions-item>
@@ -25,66 +26,35 @@
     <div class="pic">
       <div class="title">向导图</div>
 
-      <div>
-        <el-steps :active="3">
-          <el-step
-            title="步骤 1"
-            description="这是一段很长很长很长的描述性文字"
-          >
-            <template slot="description">
-              <el-image
+      <div style="width: 100%; display: flex;justify-content: space-around;background:#F5F6F7 ;padding:20px ; ">
+        <el-image
                 style="width: 240px; height: 200px;"
                 :src="require('../../assets/daotu/微信图片_20230626161033.jpg')"
                 fit="fill"
                 :preview-src-list="srcList"
               >
               </el-image>
-            </template>
-          </el-step>
-          <el-step
-            title="步骤 2"
-            description="这是一段很长很长很长的描述性文字"
-          >
-            <template slot="description">
-              <el-image
+        <el-image
                 style="width: 240px; height: 200px;"
-                :src="require('../../assets/daotu/微信图片_20230626161033.jpg')"
+                :src="require('../../assets/daotu/微信图片_20230626161033.png')"
+                fit="fill"
                 :preview-src-list="srcList"
-
               >
               </el-image>
-            </template>
-          </el-step>
-          <el-step
-            title="步骤 3"
-            description="这是一段很长很长很长的描述性文字"
-          >
-            <template slot="description">
-              <el-image
+        <el-image
                 style="width: 240px; height: 200px;"
-                :src="require('../../assets/daotu/微信图片_20230626161033.jpg')"
+                :src="require('../../assets/daotu/微信图片_202306261610331.jpg')"
+                fit="fill"
                 :preview-src-list="srcList"
-
               >
               </el-image>
-            </template>
-          </el-step>
-          <el-step
-            title="步骤 4"
-            description="这是一段很长很长很长的描述性文字"
-          >
-            <template slot="description">
-              <el-image
+        <el-image
                 style="width: 240px; height: 200px;"
-                :src="require('../../assets/daotu/微信图片_20230626161033.jpg')"
+                :src="require('../../assets/daotu/微信图片_202306261610331.png')"
+                fit="fill"
                 :preview-src-list="srcList"
-
               >
               </el-image>
-            </template>
-          </el-step>
-          
-        </el-steps>
       </div>
     </div>
   </div>
@@ -96,6 +66,9 @@ export default {
     return {
         srcList: [
           require('../../assets/daotu/微信图片_20230626161033.jpg'),
+          require('../../assets/daotu/微信图片_20230626161033.png'),
+          require('../../assets/daotu/微信图片_202306261610331.jpg'),
+          require('../../assets/daotu/微信图片_202306261610331.png'),
          
         ]
     };
@@ -104,9 +77,19 @@ export default {
 </script>
 
 <style lang="scss">
+.welcom{
+    .el-descriptions__body{
+background: none;
+}
+
+.el-descriptions--medium:not(.is-bordered) .el-descriptions-item__cell {
+
+padding-bottom: 20px;}
+}
+
 .welcom {
   padding: 20px;
-  background: #fff;
+  background:url('../../assets/daotu/welcomBgc.png');
   .welcom-label {
     color: #666;
   }
